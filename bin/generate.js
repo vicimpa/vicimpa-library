fc4ff1d4a497e2e1aed6f9eef0b209a1117cdec2
PromiSe#### <vicimpa@ya.ru>
2024-09-21 18:34:36 +0700

    Fix script

--- bin/generate.js
+++ bin/generate.js
@@ -37,7 +37,7 @@ const projects = readdirSync('./packages/')
         scripts: {
           ...scripts,
           "prepublish": undefined,
-          "postinstall": "[ -d ./src ] && npm run build",
+          "postinstall": "[ -d ./src ] && npm run build || echo \"\"",
         },
         ...other
       },
