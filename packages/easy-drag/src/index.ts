1e717a3bc2fd264ca36b027ae0186c8c86954bf1
PromiSe#### <vicimpa@ya.ru>
2025-01-01 00:28:48 +0700

    [easy-drag] Append TouchEvent

--- packages/easy-drag/src/index.ts
+++ packages/easy-drag/src/index.ts
@@ -1,5 +1,7 @@
-import { Vec2 } from "@vicimpa/lib-vec2";
+import { Vec2, vec2 } from "@vicimpa/lib-vec2";
+
 import { windowEvents } from "@vicimpa/events";
+
 export type TDragEvent = {
   start: Vec2;
   current: Vec2;
@@ -11,66 +13,92 @@ export type TDragStop<T extends any[] = []> = (e: TDragEvent, ...meta: T) => voi
 export type TDragMove<T extends any[] = []> = (e: TDragEvent, ...meta: T) => void | TDragStop<T>;
 export type TDragStart<T extends any[] = []> = (e: TDragEvent, ...meta: T) => void | TDragMove<T>;
 
+
 export const makeDrag = <T extends any[] = []>(
   dragStart: TDragStart<T>,
   btn = 0,
   fromOffset = false
-) => (e: MouseEvent | { nativeEvent: MouseEvent; }, ...meta: T) => {
-  if ('nativeEvent' in e)
-    e = e.nativeEvent;
+) => {
+  let used = false;
 
-  if (e.button !== btn)
-    return;
+  return (e: MouseEvent | TouchEvent | { nativeEvent: MouseEvent | TouchEvent; }, ...meta: T) => {
+    if (used)
+      return;
 
-  e.preventDefault();
-  e.stopPropagation();
+    used = true;
 
-  const start = fromOffset ? Vec2.fromOffsetXY(e as MouseEvent) : Vec2.fromPageXY(e);
-  const current = start.clone();
-  const delta = new Vec2(0);
+    if ('nativeEvent' in e)
+      e = e.nativeEvent;
 
-  const event = {
-    get start() {
-      return start.clone();
-    },
-    get current() {
-      return current.clone();
-    },
-    get delta() {
-      return delta.clone();
-    },
-    target: e.target
-  };
+    let id = -1;
 
-  const update = () => {
-    if (!move) return;
-    stop = move(event, ...meta);
-  };
-
-  let move = dragStart(event, ...meta);
-  let stop: TDragStop<T> | void;
-
-  update();
-
-  const unsub = [
-    windowEvents(['mouseup', 'blur'], (e) => {
-      if ('button' in e && e.button !== btn)
+    if (e instanceof MouseEvent)
+      if (e.button !== btn)
         return;
 
-      stop instanceof Function && stop(event, ...meta);
-      unsub.forEach(u => u?.());
-    }),
-    windowEvents('mousemove', (e) => {
-      if (fromOffset)
-        Vec2.fromOffsetXY(e, current);
-      else
-        Vec2.fromPageXY(e, current);
-      delta.set(start).minus(current);
-      update();
-    }),
-    () => {
-      move = undefined;
-      stop = undefined;
-    }
-  ];
+    if (e instanceof TouchEvent)
+      id = e.changedTouches[0].identifier;
+
+    const getPosition = (e: MouseEvent | TouchEvent, vec = vec2()) => {
+      if (e instanceof MouseEvent)
+        return fromOffset ? Vec2.fromOffsetXY(e, vec) : Vec2.fromPageXY(e, vec);
+
+      const find = [...e.changedTouches].find(e => e.identifier === id);
+      if (!find) return null;
+      return Vec2.fromPageXY(find, vec);
+    };
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    const start = getPosition(e)!;
+    const current = start.clone();
+    const delta = new Vec2(0);
+
+    const event = {
+      get start() {
+        return start.clone();
+      },
+      get current() {
+        return current.clone();
+      },
+      get delta() {
+        return delta.clone();
+      },
+      target: e.target
+    };
+
+    const update = () => {
+      if (!move) return;
+      stop = move(event, ...meta);
+    };
+
+    let move = dragStart(event, ...meta);
+    let stop: TDragStop<T> | void;
+
+    update();
+
+    const unsub = [
+      windowEvents(['mouseup', 'touchend', 'blur'], (e) => {
+        if (e instanceof MouseEvent && e.button !== btn)
+          return;
+
+        if (e instanceof TouchEvent && !getPosition(e))
+          return;
+
+        stop && stop(event, ...meta);
+        unsub.forEach(u => u?.());
+      }),
+      windowEvents(['mousemove', 'touchmove'], (e) => {
+        if (!getPosition(e, current)) return;
+        delta.set(start).minus(current);
+        update();
+      }),
+      () => {
+        move = undefined;
+        stop = undefined;
+        used = false;
+      }
+    ];
+  };
 };
