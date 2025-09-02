95e81e6390d53bba7fd0390f85363d8bb14afc77
PromiSe#### <vicimpa@ya.ru>
2025-09-02 15:44:58 +0300

    [@vicimpa/decorators] Fix build again

--- packages/decorators/rollup.config.js
+++ packages/decorators/rollup.config.js
@@ -1,20 +1,36 @@
 import typescript from "@rollup/plugin-typescript";
+import terser from '@rollup/plugin-terser';
+import dts from "rollup-plugin-dts";
 
-export default {
-  input: 'src/index.ts',
-  plugins: [typescript()],
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs'
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'esm'
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs'
+      },
+      {
+        file: 'dist/index.amd.js',
+        format: 'amd'
+      },
+    ],
+    plugins: [typescript(), terser()],
+    external: [
+      /@vicimpa/,
+      /node_modules/
+    ]
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.d.ts",
+      format: "es"
     },
-    {
-      file: 'dist/index.esm.js',
-      format: 'es'
-    }
-  ],
-  external: [
-    /@vicimpa/,
-    /node_modules/
-  ]
-};
+    plugins: [dts({})]
+  }
+];
