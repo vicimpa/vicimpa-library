2621863a16a9461a76fef787264b5d783f0c4361
PromiSe#### <vicimpa@ya.ru>
2025-09-04 22:13:31 +0300

    [@vicimpa/decorators] Fix real for getters

--- packages/decorators/src/lib/reactive.ts
+++ packages/decorators/src/lib/reactive.ts
@@ -1,4 +1,4 @@
-import { computed, Signal, signal } from "@preact/signals-core";
+import { computed, ReadonlySignal, Signal, signal, untracked } from "@preact/signals-core";
 
 import { makeWeekStore } from "@vicimpa/week-store";
 
@@ -6,7 +6,7 @@ const propStore = makeWeekStore(() => new Set<string | symbol>());
 const signalStore = makeWeekStore(() => ({}) as Record<string | symbol, Signal<any>>);
 
 export const reactive = <T extends object, C extends new (...args: any[]) => T>() => {
-  return (target: C) => {
+  return (target: C): any => {
     const { prototype } = target;
     const _store = propStore(prototype);
 
@@ -31,6 +31,22 @@ export const reactive = <T extends object, C extends new (...args: any[]) => T>(
   };
 };
 
+const myComputed = <T>(fn: () => T, setValue?: (v: T) => any) => {
+  const signal = computed(fn);
+
+  if (!setValue)
+    return signal;
+
+  return Object.setPrototypeOf({
+    get value() {
+      return signal.value;
+    },
+    set value(value) {
+      setValue(value);
+    }
+  }, signal);
+};
+
 export const prop = <T extends object>(
   target: T,
   key: string | symbol,
@@ -43,29 +59,48 @@ export const prop = <T extends object>(
     Object.defineProperty(target, key, {
       get() {
         const signal = this[sym] ?? (
-          this[sym] = computed(() => {
-            return getValue.call(this);
-          })
+          this[sym] = signalStore(this)[key] = myComputed(
+            () => getValue.call(this),
+            setValue ? (value) => {
+              setValue.call(this, value);
+            } : undefined
+          )
         );
 
         return signal.value;
       },
-      set(value) {
-        setValue.call(this, value);
-      }
+      ...(
+        setValue ? (
+          {
+            set(value) {
+              setValue.call(this, value);
+            }
+          }
+        ) : {}
+      )
     });
 
-    return;
+    return target as any;
   }
 
   propStore(target).add(key);
 };
 
-export const real = <T extends object, K extends keyof T>(target: T, key: K) => {
+type IfEquals<X, Y, A = X, B = never> =
+  (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2) ? A : B;
+
+type IsReadonlyProp<T, K extends keyof T> =
+  IfEquals<Pick<T, K>, Readonly<Pick<T, K>>, true, false>;
+
+type RealSignal<T extends object, K extends keyof T> =
+  IsReadonlyProp<T, K> extends true ? ReadonlySignal<T[K]> : Signal<T[K]>;
+
+export const real = <T extends object, K extends keyof T>(
+  target: T,
+  key: K
+): RealSignal<T, K> => {
+  untracked(() => target[key]);
   const store = signalStore(target);
-
-  if (!(key in store))
-    throw new Error('Can not find real signal');
-
-  return store[key as any] as Signal<T[K]>;
+  if (!(key in store)) throw new Error('Can not find real signal');
+  return store[key as any] as RealSignal<T, K>;
 };
